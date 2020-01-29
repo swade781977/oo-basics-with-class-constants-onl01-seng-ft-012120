ed7276fb755d73b142da63078b164cb838573902
Chris Wade <swade781977@yahoo.com>
2020-01-29 03:47:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do |array_brand|
-      if @brand != array_brand ? BRANDS << brand : 
+      if @brand != array_brand ? BRANDS << brand : false
       
         
       end
